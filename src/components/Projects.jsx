9624a5c3b41898509b5ea0749b88d35b197a4dff
SanthoshKumar-PS <santhoshkumarsakthi2003@gmail.com>
2025-10-24 20:29:23 +0530

    Contact and Footer updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { assets, projectsData } from "../assets/assets"
+import {motion} from "framer-motion"
 
 const Projects = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -27,7 +28,12 @@ const Projects = () => {
     },[])
 
   return (
-    <div className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden" id="Projects">
+    <motion.div 
+        initial = {{opacity:0, x:-200}}
+        transition= {{duration:1}}
+        whileInView = {{opacity:1,x:0}}
+        viewport={{once:true}}
+    className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden" id="Projects">
         <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center ">Projects  
             <span className="underline underline-offset-4 decoration-1 under font-light"> Completed</span>
         </h1>
@@ -53,7 +59,7 @@ const Projects = () => {
                         <div className="absolute left-0 right-0 bottom-5 flex justify-center">
                             <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
                                 <h2 className=" font-semibold text-gray-800">{project.title}</h2>
-                                <p className="text-gray-500 text-xs">{project.price} <span>|</span> {project.location}</p>
+                                <p className="text-gray-500 text-xs">{project.price} <span className="px-1">|</span> {project.location}</p>
 
                             </div>
                         </div>
@@ -62,7 +68,7 @@ const Projects = () => {
             </div>
         </div>
 
-    </div>
+    </motion.div>
   )
 }
 
